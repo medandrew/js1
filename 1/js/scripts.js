8143e6921839043848fa815a72ae121ca82a5783
medandrew <359747@mail.ru>
2018-01-29 03:35:45 +0700

    homework-1

---
+++ 1/js/scripts.js
@@ -0,0 +1,63 @@
+window.addEventListener('load', function(e){
+   
+    var num1 = document.querySelector('.num1');
+    var num2 = document.querySelector('.num2');
+    var operator = document.querySelector('.operator');
+    var result = document.querySelector('.res');
+    var go = document.querySelector('.go');
+
+    num1.addEventListener('input', function(){
+        buttonUnblock();
+        prepare(num1);
+    });
+
+    num2.addEventListener('input', function(){
+        buttonUnblock();
+        prepare(num2);
+    });
+
+    operator.oninput = buttonUnblock;
+
+    go.addEventListener('click', function(){
+        if ('' != num1.value && '' != num2.value) {
+            calculate(operator.value);
+            this.disabled = true;
+        } else {
+            result.innerHTML = 'Введите оба числа';
+        }
+    });
+
+
+
+    function buttonUnblock() {
+        go.disabled = false;
+    }
+
+    function prepare(obj) {
+        obj.value = obj.value.replace(/\D/g, '');
+    }
+
+    function calculate(operator) {
+        switch (operator) {
+            case '+':
+                res = parseInt(num1.value) + parseInt(num2.value);
+                break;
+            case '-':
+                res = parseInt(num1.value) - parseInt(num2.value);
+                break;
+            case '*':
+                res = parseInt(num1.value) * parseInt(num2.value);
+                break;
+            case '/':
+                if (num2.value == 0) {
+                    res = 'Делить на 0 не лучшее решение';
+                } else {
+                    res = parseInt(num1.value) / parseInt(num2.value);
+                }
+                break;
+            default:
+                res = 'Неверный оператор';
+        }
+        result.innerHTML = res;
+    }
+});
